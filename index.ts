e382e6ffcf91e3a2fb63f53aaa3224993f9e7c65
CodeSandbox <support@codesandbox.io>
2022-11-30 17:00:02 +0000

    Initial commit

---
+++ index.ts
@@ -0,0 +1,26 @@
+import * as dotenv from "dotenv";
+dotenv.config({});
+import * as express from "express";
+import * as path from "path";
+import * as cors from "cors";
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: express.Response) => {
+  res.json({ idiot: "idiot" });
+});
+
+function main() {
+  try {
+    app.listen(process.env.SERVER_PORT, () =>
+      console.log(`Server started on port ${process.env.SERVER_PORT}`)
+    );
+  } catch (e) {
+    console.log(e.message);
+  }
+}
+
+main();
+
+export default main;
